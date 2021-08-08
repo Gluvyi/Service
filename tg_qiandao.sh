8b4db29585b8d85bd8ff2f60ed9c0d5fd0889e51
Yu1zzZ <60965636+Yu1zzZ@users.noreply.github.com>
2021-08-08 23:21:35 +0800

    Delete tg_qiandao.sh

--- tg_qiandao.sh
+++
@@ -1,5 +0,0 @@
-#######卷毛鼠,厂妹签到
-pkill telegram-cli
-(echo "contact_list";sleep 5;echo "msg 卷毛鼠活动机器人 /checkin"; echo "safe_quit") | proxychains4  /tg/bin/telegram-cli -k tg-server.pub -W & (echo "contact_list";sleep 5;echo "msg 大河马 /checkin"; echo "safe_quit") | proxychains4  /tg/bin/telegram-cli -k tg-server.pub -W & 
-sleep 100s
-pkill telegram-cli
